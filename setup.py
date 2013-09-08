b1f684170eaa3c0f2aa7a5f763ce3e0785d9d1b0
dhagrow <cymrow@gmail.com>
2013-09-08 08:25:10 -0400

    tagged for first release

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='fontview', version='0.1',
-    description='Font Previewer',
+    description='A font and styling previewer for programmers.',
     author='Miguel Turner', author_email='cymrow at gmail.com',
-    url='http://fontview-dhagrow.rhcloud.com',
+    url='http://fonts.dhagrow.org/',
     
     install_requires=[
         'bottle==0.11.6',
